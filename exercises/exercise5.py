b469fb0c596de1f119f0ce096b25e6ec674e16e9
manuelibarbia <manuelibarbia02@gmail.com>
2022-04-07 19:09:00 -0300

    Ejercicio 5 resuelto

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -11,7 +11,7 @@ variable_02 = "días "
 variable_03 = "a todos!"
 
 # COMPLETAR - INICIO
-
+strings_concatenados = variable_01 + variable_02 + variable_03
 # COMPLETAR - FIN
 
 assert strings_concatenados == "¡Buenos días a todos!"
@@ -28,7 +28,10 @@ usar operadores).
 # "strings!"
 
 # COMPLETAR - INICIO
-
+frase_1 = "¡Mamá "
+frase_2 = "estoy concatenando "
+frase_3 = "strings!"
+strings_concatenados = "{}{}{}".format(frase_1,frase_2,frase_3)
 # COMPLETAR - FIN
 
 assert strings_concatenados == "¡Mamá estoy concatenando strings!"
@@ -45,7 +48,9 @@ variable_02 = 600
 variable_03 = " pesos a un amigo."
 
 # COMPLETAR - INICIO
+variable_02 = str(variable_02)
 
+strings_concatenados = "{}".format(variable_01+variable_02+variable_03)
 # COMPLETAR - FIN
 
 assert strings_concatenados == "Le debo 600 pesos a un amigo."
@@ -64,12 +69,10 @@ variable_04 = " años."
 variable_05 = "Ezequiel"
 
 # COMPLETAR - INICIO
-
+strings_concatenados = "{}{}{}{}{} Se llama {}".format(variable_01,variable_02,variable_03,variable_02,variable_04,variable_05)
 # COMPLETAR - FIN
 
-assert (
-    strings_concatenados == "Le debo 6 pesos a un amigo hace 6 años. Se llama Ezequiel"
-)
+assert strings_concatenados == "Le debo 6 pesos a un amigo hace 6 años. Se llama Ezequiel"
 
 
 """
@@ -83,7 +86,7 @@ variable_03 = " pesos que le debía a Ezequiel, me faltan $"
 variable_04 = 4
 
 # COMPLETAR - INICIO
-
+strings_concatenados = f'{variable_01}{variable_02}{variable_03}{variable_04}'
 # COMPLETAR - FIN
 
 assert strings_concatenados == "Le pagué 2 pesos que le debía a Ezequiel, me faltan $4"
